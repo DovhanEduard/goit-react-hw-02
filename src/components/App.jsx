ed1420d24a7c1d08c5756d506d39ab3a37b9b0ea
Dovhan_Eduard <dovhan.eduard@gmail.com>
2024-07-28 16:43:00 +0300

    complete hm 2 react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,68 @@
-import './App.css'
-import Profile from './Profile/Profile.jsx'
-import userData from "../userData.json"
-import FriendList from './FriendList/FriendList.jsx'
-import friendsList from "../friendsList.json"
-import TransactionHistory from './TransactionHistory/TransactionHistory.jsx'
-import transactions from "../transactions.json"
-
+import './App.css';
+import { useState, useEffect } from 'react';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
 
 function App() {
+  const [reviews, setReviews] = useState(() => {
+    const isLocalStorageHasData = Boolean(localStorage.getItem('reviews'));
+
+    if (isLocalStorageHasData) {
+      const data = localStorage.getItem('reviews');
+      return JSON.parse(data);
+    }
+
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  const totalFeedback = reviews.good + reviews.neutral + reviews.bad;
+  const positiveFeedback = Math.round((reviews.good / totalFeedback) * 100);
+
+  useEffect(() => {
+    if (totalFeedback !== 0) {
+      const data = JSON.stringify(reviews);
+      console.log(data);
+      localStorage.setItem('reviews', data);
+    }
+  }, [reviews, totalFeedback]);
+
+  const updateFeedback = feedbackType => {
+    if (feedbackType === 'reset') {
+      setReviews({ good: 0, neutral: 0, bad: 0 });
+      localStorage.removeItem('reviews');
+    } else {
+      setReviews({ ...reviews, [feedbackType]: reviews[feedbackType] + 1 });
+    }
+  };
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
 
-      <FriendList friends = {friendsList} />
-      <TransactionHistory items={transactions} />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+
+      {totalFeedback === 0 && <Notification />}
+
+      {totalFeedback !== 0 && (
+        <Feedback
+          good={reviews.good}
+          neutral={reviews.neutral}
+          bad={reviews.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
     </>
-    
-  )
+  );
 }
 
 export default App;
